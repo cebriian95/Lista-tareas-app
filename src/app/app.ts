ad83f79e9c88498976303a3e687510478b8edc37
cebriian95 <cebriian95@proton.me>
2026-02-17 11:19:41 +0100

    proyecto de app tareas terminado

---
+++ src/app/app.ts
@@ -0,0 +1,42 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Tareas } from './services/tareas';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,
+    FormsModule
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+
+  listaTareas: string[] = [];
+  nuevaTarea: string = '';
+
+  private _tareas = inject(Tareas);
+
+  ngOnInit() {
+    this.listaTareas = this._tareas.getTareas();
+  }
+
+  agregarTarea() {
+    if (this.nuevaTarea.trim() !== '') {
+      this._tareas.agregarTarea(this.nuevaTarea);
+      this.listaTareas = this._tareas.getTareas();
+      this.nuevaTarea = '';
+    }
+  }
+
+  eliminarTarea(index: number) {
+    this._tareas.eliminarTarea(index);
+    this.listaTareas = this._tareas.getTareas();
+  }
+
+
+
+
+
+}
